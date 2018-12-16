b1ae996e74851e6cda8720f2ff4705a7308c0646
Vova Abdrakhmanov <369565@gmail.com>
2018-12-16 15:52:00 +0400

    Add game model

--- game.py
+++ game.py
@@ -1,26 +1,50 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from auth import login_required
 
 games = Blueprint('games', __name__, template_folder='templates')
 
+class Game:
+    last_id = 0
+    storage = {}
+    def __init__(self, private=False):
+        Game.last_id += 1
+        self.id = Game.last_id
+        self.status = 'pending'
+        self.players = []
+        self.state = {}
+        self.private = private
+        Game.storage[self.id] = self
+
+
 @games.route('/new')
 @login_required
-def new(id):
-    pass
+def new():
+    return jsonify(Game().__dict__)
 
 @games.route('/pending')
 def pending(id):
-    pass
+    games_pending = {k: v.__dict__ for k, v in Game.storage.items()
+                                if v.status == 'pending'}
+    return jsonify(games_pending)
 
-@games.route('/<int:id>/invite/<hash>')
+@games.route('/current')
 @login_required
-def invite(id, hash):
+def current(id):
+    games_current = {k: v.__dict__ for k, v in Game.storage.items()
+                                if v.status == 'current'}
+    return jsonify(games_current)
+
+@games.route('/<int:id>/invite/<int:user_id>')
+@login_required
+def invite(id, user_id):
     pass
 
 @games.route('/<int:id>/join')
+@games.route('/<int:id>/join/<hash>')
 @login_required
-def join(id):
+def join(id, hash = None):
     pass
+
 
 @games.route('/<int:id>/leave')
 @login_required
@@ -30,7 +54,7 @@ def leave(id):
 @games.route('/<int:id>/players')
 @login_required
 def players(id):
-    pass
+    return jsonify(Game.storage[id].players)
 
 @games.route('/<int:id>/start')
 @login_required
@@ -45,7 +69,7 @@ def close(id):
 @games.route('/<int:id>/state')
 @login_required
 def state(id):
-    pass
+    return jsonify(Game.storage[id].state)
 
 @games.route('/<int:id>/contract')
 @login_required
